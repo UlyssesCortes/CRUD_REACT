9e85a92158c343ffc13fa2d7e3861f523b479ff5
Ulysses Cortes <ulyssescortes13@gmail.com>
2022-12-12 21:21:38 -0600

    CRUD compleated

---
+++ pre-work/src/Create.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Create = ({ posts, setPosts }) => {
+    const [title, setTitle] = useState([])
+    const [body, setBody] = useState([])
+
+    const handleSubmit = async (ev) => {
+        ev.preventDefault();
+        // console.log('title, body', title, body)
+        const response = await fetch('https://jsonplace-univclone.herokuapp.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'Application/json',
+            },
+            body: JSON.stringify({
+                title,
+                body,
+            })
+        });
+        const data = await response.json()
+        console.log('data', data)
+        setPosts([data, ...posts])
+        setTitle('')
+        setBody('')
+    }
+
+    return <>
+        <h3>Create a Post</h3>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='title' value={title} onChange={(ev) => setTitle(ev.target.value)}></input>
+            <input type="text" placeholder='body' value={body} onChange={(ev) => setBody(ev.target.value)}></input>
+            <button type='submit' className='btn btn-outline-primary'>Submit</button>
+        </form>
+    </>
+}
+
+export default Create;
---
+++ pre-work/src/index.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import Create from './Create';
+import Update from './Update';
+
+const App = () => {
+  const [posts, setPosts] = useState([])
+  const [postId, setPostId] = useState(null)
+  console.log('posts', posts)
+
+  const handleDelete = async (postIdToDelete) => {
+    const response = await fetch(`https://jsonplace-univclone.herokuapp.com/posts/${postIdToDelete}`, {
+      method: 'DELETE',
+    })
+
+
+    const data = await response.json()
+    if (data) {
+      const newPosts = posts.filter(post => post.id !== postIdToDelete)
+      setPosts(newPosts)
+    }
+  }
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const resp = await fetch('https://jsonplace-univclone.herokuapp.com/posts')
+      const data = await resp.json();
+      setPosts(data);
+    }
+    fetchPosts()
+  }, [])
+
+  return <>
+    <h1>
+      {
+        postId
+          ? <Update posts={posts} setPosts={setPosts} postId={postId} />
+          : <Create posts={posts} setPosts={setPosts} />
+
+      }
+    </h1>
+    {
+      posts.map(post => <div key={post.id}>
+        <h3>{post.title}</h3>
+        <div>{post.body}</div>
+        <button onClick={() => setPostId(post.id)}>
+          Edit
+        </button>
+        <button onClick={() => handleDelete(post.id)}>
+          Delete
+        </button>
+
+      </div>)
+    }
+  </>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
